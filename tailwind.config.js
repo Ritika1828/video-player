97fa9bd29285c63fe5828e62cd67fde6669e2213
Ritika1828 <ritika.juneja@byjus.com>
2024-02-18 22:39:47 +0530

    animation fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  prefix: "video-",
+  prefix: 'video-',
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeOut: {
+          '0%': { opacity: 1},
+          '100%': { opacity: 0,
+            transform: 'scale(2)'
+            },
+        },
+      },
+      animation: {
+        'fadeout': 'fadeOut 0.5s linear 1 normal forwards',
+      },
+    },
   },
   plugins: [],
 }
